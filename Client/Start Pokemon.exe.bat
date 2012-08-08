59690b72f6dd587e5e49e73c7818707143bf7cd2
myth1c <myth1c@06608f2d-14d8-424a-8bde-90b1561898ce>
2012-08-08 22:20:22 +0000

    Changed the bat file to prefer 1.6 (x64 or x86) over 1.7, since 1.7 has issues with the battlescreen.

--- Client/Start Pokemon.exe.bat
+++ Client/Start Pokemon.exe.bat
@@ -2,10 +2,10 @@
 SET LIBRARY_PATH=-Djava.library.path=lib\native
 SET CLIENT=betaclient.jar
 
-IF EXIST "%ProgramFiles%\Java\jre7" goto Java1.7
 IF EXIST "%ProgramFiles%\Java\jre6" goto Java1.6
-IF EXIST "%ProgramFiles(x86)%\Java\jre7" goto Java1.7x32
 IF EXIST "%ProgramFiles(x86)%\Java\jre6" goto Java1.6x32
+IF EXIST "%ProgramFiles%\Java\jre7" goto Java1.7
+IF EXIST "%ProgramFiles(x86)%\Java\jre7" goto Java1.76x32
 
 :Java1.7	
 "%ProgramFiles%\Java\jre7\bin\java.exe" %LIBRARY_PATH% -jar %CLIENT%

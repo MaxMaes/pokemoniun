c9bff8792bffaf6039db63efb6b5c1bc34702825
remyoman <remyoman@06608f2d-14d8-424a-8bde-90b1561898ce>
2013-01-24 19:49:59 +0000

    Added LoggedElswhere message to packets and SVN :)

---
+++ Client/src/org/pokenet/client/messages/events/LoggedElsewhereEvent.java
@@ -0,0 +1,19 @@
+package org.pokenet.client.messages.events;
+
+import org.pokenet.client.GameClient;
+import org.pokenet.client.Session;
+import org.pokenet.client.messages.MessageEvent;
+import org.pokenet.client.protocol.ClientMessage;
+import org.pokenet.client.protocol.ServerMessage;
+
+public class LoggedElsewhereEvent implements MessageEvent
+{
+
+	@Override
+	public void parse(Session session, ServerMessage request, ClientMessage message)
+	{
+		GameClient.messageDialog("You seem to be logged onto another server, please log out there first.", GameClient.getInstance().getDisplay());
+		GameClient.getInstance().getLoadingScreen().setVisible(false);
+		GameClient.getInstance().getLoginScreen().showLogin();
+	}
+}

b4c46a74330320865ec6c3b833199c2693b7bc48
sadhi <sadhi@06608f2d-14d8-424a-8bde-90b1561898ce>
2013-02-08 13:51:55 +0000

    [FIX] map errors

--- Server/res/maps/normal12.tsx
+++ Server/res/maps/normal12.tsx
@@ -2,13 +2,13 @@
  <tileset name="1" firstgid="1" tilewidth="16" tileheight="16">
   <image source="normal12.png" trans="000000"/>
   <tile id="579">
-  <properties>
-   <property name="Grass" value="true"/>
-  </properties>
- </tile>
- <tile id="769">
-  <properties>
-   <property name="Grass" value="true"/>
-  </properties>
- </tile>
+   <properties>
+    <property name="Grass" value="true"/>
+   </properties>
+  </tile>
+  <tile id="769">
+   <properties>
+    <property name="Grass" value="true"/>
+   </properties>
+  </tile>
  </tileset>

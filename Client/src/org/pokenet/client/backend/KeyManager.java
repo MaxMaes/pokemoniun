e2644b3dd470c752958bc3c367ed1f5accd21e81
myth1c <myth1c@06608f2d-14d8-424a-8bde-90b1561898ce>
2013-02-24 10:53:58 +0000

    Updated slick to latest version. Edited the keymanager to use fancy java 1.7 string switch.

--- Client/src/org/pokenet/client/backend/KeyManager.java
+++ Client/src/org/pokenet/client/backend/KeyManager.java
@@ -206,134 +206,72 @@ public class KeyManager
 	 */
 	private static Integer stringToInt(String st)
 	{
-		if(st.equals("KEY_UP"))
-			return Input.KEY_UP;
-		else if(st.equals("KEY_LEFT"))
-			return Input.KEY_LEFT;
-		else if(st.equals("KEY_RIGHT"))
-			return Input.KEY_RIGHT;
-		else if(st.equals("KEY_DOWN"))
-			return Input.KEY_DOWN;
-		else if(st.equals("A"))
-			return Input.KEY_A;
-		else if(st.equals("B"))
-			return Input.KEY_B;
-		else if(st.equals("C"))
-			return Input.KEY_C;
-		else if(st.equals("D"))
-			return Input.KEY_D;
-		else if(st.equals("E"))
-			return Input.KEY_E;
-		else if(st.equals("F"))
-			return Input.KEY_F;
-		else if(st.equals("G"))
-			return Input.KEY_G;
-		else if(st.equals("H"))
-			return Input.KEY_H;
-		else if(st.equals("I"))
-			return Input.KEY_I;
-		else if(st.equals("J"))
-			return Input.KEY_J;
-		else if(st.equals("K"))
-			return Input.KEY_K;
-		else if(st.equals("L"))
-			return Input.KEY_L;
-		else if(st.equals("M"))
-			return Input.KEY_M;
-		else if(st.equals("N"))
-			return Input.KEY_N;
-		else if(st.equals("O"))
-			return Input.KEY_O;
-		else if(st.equals("P"))
-			return Input.KEY_P;
-		else if(st.equals("Q"))
-			return Input.KEY_Q;
-		else if(st.equals("R"))
-			return Input.KEY_R;
-		else if(st.equals("S"))
-			return Input.KEY_S;
-		else if(st.equals("T"))
-			return Input.KEY_T;
-		else if(st.equals("U"))
-			return Input.KEY_U;
-		else if(st.equals("V"))
-			return Input.KEY_V;
-		else if(st.equals("W"))
-			return Input.KEY_W;
-		else if(st.equals("X"))
-			return Input.KEY_X;
-		else if(st.equals("Y"))
-			return Input.KEY_Y;
-		else if(st.equals("Z"))
-			return Input.KEY_Z;
-		else if(st.equals("LSHIFT"))
-			return Input.KEY_LSHIFT;
-		else if(st.equals("LALT"))
-			return Input.KEY_LALT;
-		else if(st.equals("RSHIFT"))
-			return Input.KEY_RSHIFT;
-		else if(st.equals("RALT"))
-			return Input.KEY_RALT;
-		else if(st.equals("RCTRL"))
-			return Input.KEY_RCONTROL;
-		else if(st.equals("LCTRL"))
-			return Input.KEY_LCONTROL;
-		else if(st.equals("ENTER"))
-			return Input.KEY_ENTER;
-		else if(st.equals("NUMPADENTER"))
-			return Input.KEY_NUMPADENTER;
-		else if(st.equals("HOME"))
-			return Input.KEY_HOME;
-		else if(st.equals("END"))
-			return Input.KEY_END;
-		else if(st.equals("0"))
-			return Input.KEY_0;
-		else if(st.equals("1"))
-			return Input.KEY_1;
-		else if(st.equals("2"))
-			return Input.KEY_2;
-		else if(st.equals("3"))
-			return Input.KEY_3;
-		else if(st.equals("4"))
-			return Input.KEY_4;
-		else if(st.equals("5"))
-			return Input.KEY_5;
-		else if(st.equals("6"))
-			return Input.KEY_6;
-		else if(st.equals("7"))
-			return Input.KEY_7;
-		else if(st.equals("8"))
-			return Input.KEY_7;
-		else if(st.equals("9"))
-			return Input.KEY_7;
-		else if(st.equals("0"))
-			return Input.KEY_7;
-		else if(st.equals("-"))
-			return Input.KEY_MINUS;
-		else if(st.equals("+"))
-			return Input.KEY_ADD;
-		else if(st.equals("BACKSPACE"))
-			return Input.KEY_BACK;
-		else if(st.equals("TAB"))
-			return Input.KEY_TAB;
-		else if(st.equals(';'))
-			return Input.KEY_SEMICOLON;
-		else if(st.equals('.'))
-			return Input.KEY_PERIOD;
-		else if(st.equals(','))
-			return Input.KEY_COMMA;
-		else if(st.equals('='))
-			return Input.KEY_EQUALS;
-		else if(st.equals('['))
-			return Input.KEY_LBRACKET;
-		else if(st.equals(']'))
-			return Input.KEY_RBRACKET;
-		else if(st.equals('/'))
-			return Input.KEY_SLASH;
-		else if(st.equals("DEL"))
-			return Input.KEY_DELETE;
-		else if(st.equals("SPACE"))
-			return Input.KEY_SPACE;
-		return null;
+		switch(st)
+		{
+			case "KEY_UP": return Input.KEY_UP;
+			case "KEY_LEFT": return Input.KEY_LEFT;
+			case "KEY_RIGHT": return Input.KEY_RIGHT;
+			case "KEY_DOWN": return Input.KEY_UP;
+			case "A": return Input.KEY_A;
+			case "B": return Input.KEY_B;
+			case "C": return Input.KEY_C;
+			case "D": return Input.KEY_D;
+			case "E": return Input.KEY_E;
+			case "F": return Input.KEY_F;
+			case "G": return Input.KEY_G;
+			case "H": return Input.KEY_H;
+			case "I": return Input.KEY_I;
+			case "J": return Input.KEY_J;
+			case "K": return Input.KEY_K;
+			case "L": return Input.KEY_L;
+			case "M": return Input.KEY_M;
+			case "N": return Input.KEY_N;
+			case "O": return Input.KEY_O;
+			case "P": return Input.KEY_P;
+			case "Q": return Input.KEY_Q;
+			case "R": return Input.KEY_R;
+			case "S": return Input.KEY_S;
+			case "T": return Input.KEY_T;
+			case "U": return Input.KEY_U;
+			case "V": return Input.KEY_V;
+			case "W": return Input.KEY_W;
+			case "X": return Input.KEY_X;
+			case "Y": return Input.KEY_Y;
+			case "Z": return Input.KEY_Z;
+			case "LSHIFT": return Input.KEY_LSHIFT;
+			case "LALT": return Input.KEY_LALT;
+			case "RSHIFT": return Input.KEY_RSHIFT;
+			case "RALT": return Input.KEY_RALT;
+			case "RCTRL": return Input.KEY_RCONTROL;
+			case "LCTRL": return Input.KEY_LCONTROL;
+			case "ENTER": return Input.KEY_ENTER;
+			case "NUMPADENTER": return Input.KEY_NUMPADENTER;
+			case "HOME": return Input.KEY_HOME;
+			case "END": return Input.KEY_END;
+			case "0": return Input.KEY_0;
+			case "1": return Input.KEY_1;
+			case "2": return Input.KEY_2;
+			case "3": return Input.KEY_3;
+			case "4": return Input.KEY_4;
+			case "5": return Input.KEY_5;
+			case "6": return Input.KEY_6;
+			case "7": return Input.KEY_7;
+			case "8": return Input.KEY_8;
+			case "9": return Input.KEY_9;
+			case "-": return Input.KEY_MINUS;
+			case "+": return Input.KEY_ADD;
+			case "BACKSPACE": return Input.KEY_BACK;
+			case "TAB": return Input.KEY_TAB;
+			case ";": return Input.KEY_SEMICOLON;
+			case ".": return Input.KEY_PERIOD;
+			case ",": return Input.KEY_COMMA;
+			case "=": return Input.KEY_EQUALS;
+			case "[": return Input.KEY_LBRACKET;
+			case "]": return Input.KEY_RBRACKET;
+			case "/": return Input.KEY_SLASH;
+			case "DEL": return Input.KEY_DELETE;
+			case "SPACE": return Input.KEY_SPACE;
+			default: return null;
+		}
 	}
 }

8f0300b96a84a236681705909fc919ec7f02ec1e
myth1c <myth1c@06608f2d-14d8-424a-8bde-90b1561898ce>
2013-03-15 09:37:28 +0000

    Added events from SUI, they should work fine. If not, we'll edit them a bit :)

---
+++ Client/src/org/pokenet/client/ui/events/Listener.java
@@ -0,0 +1,12 @@
+package org.pokenet.client.ui.events;
+
+import java.util.EventListener;
+
+/**
+ * A tagging interface which all SUI event listeners should extend.
+ *
+ * @author davedes
+ * @since b.0.2
+ */
+public interface Listener extends EventListener {
+}
---
+++ Client/src/org/pokenet/client/ui/events/ResizeListener.java
@@ -0,0 +1,23 @@
+package org.pokenet.client.ui.events;
+
+import mdes.slick.sui.Component;
+
+/**
+ * An interface to receive resize event
+ *
+ * @author Olivier Arteau
+ */
+
+public interface ResizeListener  extends Listener {
+    /**
+     * Notification that the component has change size
+     *
+     * @param oldWidth
+     * 		The old width of the component
+     * 
+     * @param oldHeight
+     * 		The old height of the component
+     */
+    
+    public void sizeChanged(Component c, float oldWidth, float oldHeight);
+}

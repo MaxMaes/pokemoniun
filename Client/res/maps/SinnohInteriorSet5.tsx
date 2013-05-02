ca95f6ae8f0b3c711498c1d00545b168ff843d62
teepoita <teepoita@06608f2d-14d8-424a-8bde-90b1561898ce>
2013-05-02 17:44:14 +0000

    -fixed mt pyre crash -fixed sinnoh battle frontier crash -fixed stark mountain warp -fixed sinnoh island maps misplacements

---
+++ Client/res/maps/SinnohInteriorSet5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="SinnohInteriorSet45" firstgid="3073" tilewidth="16" tileheight="16">
+ <image source="SinnohInteriorSet5.png" trans="000000"/>
+</tileset>

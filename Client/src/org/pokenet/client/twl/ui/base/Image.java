a3238357a771970c1701d3be9595a22d44b0d7e3
myth1c <myth1c@06608f2d-14d8-424a-8bde-90b1561898ce>
2013-05-12 11:45:43 +0000

    Image widget changes size with the given image.

--- Client/src/org/pokenet/client/twl/ui/base/Image.java
+++ Client/src/org/pokenet/client/twl/ui/base/Image.java
@@ -2,14 +2,13 @@ package org.pokenet.client.twl.ui.base;
 
 import org.pokenet.client.backend.FileLoader;
 import de.matthiasmann.twl.GUI;
-import de.matthiasmann.twl.Widget;
 
 /**
  * This way I can add images as widgets, probably easier than always overriding paintWidget.
  * 
  * @author Myth1c
  */
-public class Image extends Widget
+public class Image extends de.matthiasmann.twl.Widget
 {
 	private de.matthiasmann.twl.renderer.Image img;
 
@@ -25,6 +24,7 @@ public class Image extends Widget
 
 	/**
 	 * Loads the image from the given path. The parameter cannot be null.
+	 * It also adjusts the size to the given image.
 	 * 
 	 * @param path
 	 */
@@ -33,6 +33,7 @@ public class Image extends Widget
 		if(img != null)
 		{
 			img = FileLoader.loadImage(path);
+			setSize(img.getWidth(), img.getHeight());
 		}
 		else
 		{
@@ -42,6 +43,7 @@ public class Image extends Widget
 
 	/**
 	 * Creates a widget image from the given renderer image. The parameter cannot be null.
+	 * It also adjusts the size to the given image.
 	 * 
 	 * @param path
 	 */
@@ -50,6 +52,7 @@ public class Image extends Widget
 		if(image != null)
 		{
 			img = image;
+			setSize(img.getWidth(), img.getHeight());
 		}
 		else
 		{
@@ -59,20 +62,23 @@ public class Image extends Widget
 
 	/**
 	 * Sets the image. Can be null.
+	 * It also adjusts the size to the given image.
 	 * 
 	 * @param image
 	 */
 	public void setImage(de.matthiasmann.twl.renderer.Image image)
 	{
 		img = image;
+		setSize(img.getWidth(), img.getHeight());
 	}
 
 	@Override
 	public void paintWidget(GUI gui)
 	{
+		super.paintWidget(gui);
 		if(img != null)
 		{
-			img.draw(getAnimationState(), getX(), getY());
+			img.draw(getAnimationState(), getX(), getY(), getWidth(), getHeight());
 		}
 	}
 }
